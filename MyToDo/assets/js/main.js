5cfe9efd767e6d98a8b66e37b31bc88951a144b4
20taxitaxi00 <takushi0201@icloud.com>
2020-11-01 00:34:31 +0900

    基本的な動作はOK 次回はローカルストレージに保存できるような仕組み作り

---
+++ MyToDo/assets/js/main.js
@@ -0,0 +1,94 @@
+// チェックマークとゴミ箱マークと追加マークのアイコン
+let doneIcon = '<i class="far fa-check-square fa-2x"></i>';
+let removeIcon = '<i class="far fa-trash-alt fa-2x"></i>';
+let addBtn = document.getElementById("add");
+let data;
+
+// ローカルストレージの処理
+// データがあったら
+if (localStorage.getItem("todoList")){
+	// データを取り出して、配列dataに代入
+	data = JSON.parse(localStorage.getItem("todoList"));
+// なかったら
+} else {
+	// 空の配列を代入する
+	data = [];
+}
+
+// addボタンの処理
+addBtn.addEventListener("click", function(){
+	let task_input = document.getElementById("input");
+	if (task_input != ""){
+		add_li_tag(task_input.value);
+		localStorage.setItem("todoList", JSON.stringify(data));
+		task_input.value = "";
+	}
+});
+
+
+
+// ----- 関数 -----
+
+// 追加マークを押した時
+function add_li_tag(task_text){
+	let todo_list = document.getElementById("not-yet");
+	// liタグ作成
+	let new_li = document.createElement("li");
+	new_li.textContent = task_text;
+	// divタグ作成
+	let div_button = document.createElement("div");
+	div_button.classList.add("buttons");
+	// 完了ボタン作成
+	let done = document.createElement("button");
+	done.classList.add("done");
+	done.innerHTML = doneIcon;
+	done.addEventListener("click", doneTask);
+	// 削除ボタン作成
+	let remove = document.createElement("button");
+	remove.classList.add("remove");
+	remove.innerHTML = removeIcon;
+	remove.addEventListener("click", removeTask);
+	// それぞれの要素に追加
+	div_button.appendChild(done);
+	div_button.appendChild(remove);
+	new_li.appendChild(div_button);
+	todo_list.appendChild(new_li);
+}
+
+// 完了マークを押した時
+function doneTask(){
+	let task = this.parentNode.parentNode;
+	let id = task.parentNode.id;
+	if (id !== "not-yet") {
+		return;
+	}
+	let value = task.textContent;	
+
+	// doneに追加
+	let target = document.getElementById("done");
+	target.insertBefore(task, target.childNodes[0]);
+	};
+
+//削除マークを押した時 
+function removeTask(){
+	let hantei = confirm("本当に削除しますか？");
+	if (hantei == true){
+		// 画面から削除
+		let task = this.parentNode.parentNode;
+		let id = task.parentNode.id;
+		let value = task.textContent;
+		task.remove();
+		// task = task.replace('Delete',"");
+		// data.splice(data.indexO(task),1);
+		// localStorage.setItem("todoList", JSON.stringify(data));	
+	}
+};
+
+
+
+
+
+
+
+
+
