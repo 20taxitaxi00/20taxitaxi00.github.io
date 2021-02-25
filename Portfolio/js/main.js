5a8db58daf5c1405961b47035ff9e1865996b5c1
20taxitaxi00 <takushi0201@icloud.com>
2021-02-25 21:31:14 +0900

    no message

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -51,12 +51,12 @@ $(function(){
     }
     
     // 最初にロゴがfade
-    // setTimeout(function(){
-		// $('.logo-fade p').fadeIn(1000);
-    // },500);
-    // setTimeout(function(){
-		// $('.logo-fade').fadeOut(800);
-    // },2500);
+    setTimeout(function(){
+		$('.logo-fade p').fadeIn(1000);
+    },500);
+    setTimeout(function(){
+		$('.logo-fade').fadeOut(800);
+    },2500);
 
     // タブメニュー
     $('.brands-list a').on('click', function(){
@@ -116,18 +116,18 @@ $(function(){
 });
 
 // スライダー
-var mySwiper = new Swiper ('.swiper-container', {
-  loop: true,
-  slidesPerView: 2,
-  spaceBetween: 0,
-  centeredSlides : true,
-  pagination: '.swiper-pagination',
-  nextButton: '.swiper-button-next',
-  prevButton: '.swiper-button-prev',
-  breakpoints: {
-    767: {
-      slidesPerView: 1,
-      spaceBetween: 0
-    }
-  }
-})
+// var mySwiper = new Swiper ('.swiper-container', {
+//   loop: true,
+//   slidesPerView: 2,
+//   spaceBetween: 0,
+//   centeredSlides : true,
+//   pagination: '.swiper-pagination',
+//   nextButton: '.swiper-button-next',
+//   prevButton: '.swiper-button-prev',
+//   breakpoints: {
+//     767: {
+//       slidesPerView: 1,
+//       spaceBetween: 0
+//     }
+//   }
+// })
